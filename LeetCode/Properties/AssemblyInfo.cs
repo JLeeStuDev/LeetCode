3a63f55ac9ff76bb55e75903583554a70f2c17ba
JonathanLee <jlee.devspace@gmail.com>
2025-08-08 12:29:17 -0400

    Added Assembly Information to project

---
+++ LeetCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly
+[assembly: AssemblyTitle("LeetCode")]
+[assembly: AssemblyDescription("YouTube + GitHub LeetCode Solutions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonathan Lee")]
+[assembly: AssemblyProduct("LeetCode Project")]
+
+[assembly: ComVisible(false)]
+
+// This is your unique GUID for COM — not super important unless doing COM interop
+[assembly: Guid("d13a1f90-2c24-4c8a-88b8-123456789abc")]
+
+// Versioning
+// Major Version, Minor Version, Build Number, Revision
+// Example: [assembly: AssemblyVersion("1.0.0.0")]
+// You can also use "*" for auto-build/revision numbers.
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
