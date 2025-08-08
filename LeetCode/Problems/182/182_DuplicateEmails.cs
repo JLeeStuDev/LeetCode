f4ba36b23683cf67616dbb79db0ca65b41708606
JonathanLee <jlee.devspace@gmail.com>
2025-08-08 15:17:44 -0400

    Added logic to emulate SQL with no database

--- LeetCode/Problems/182/182_DuplicateEmails.cs
+++ LeetCode/Problems/182/182_DuplicateEmails.cs
@@ -1,37 +1,30 @@
-﻿using System;
-using System.Collections.Generic; 
-using System.Linq;
-
-namespace LeetCode.Problems._182
+﻿namespace LeetCode.Problems._182
 {
-    public class emailList
+    public class EmailEntry
     {
         public int Id { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
     }
 
-    static void Run()
+    public static class _182_DuplicateEmails
+    {
+        public static void Run()
         {
-            // Example input data (same as LeetCode #182 example)
-            List<emailList> people = new List<emailList>
-        {
-            new emailList { Id = 1, Email = "a@b.com" },
-            new emailList { Id = 2, Email = "c@d.com" },
-            new emailList { Id = 3, Email = "a@b.com" }
-        };
+            var people = new List<EmailEntry>
+            {
+                new EmailEntry { Id = 1, Email = "a@b.com" },
+                new EmailEntry { Id = 2, Email = "c@d.com" },
+                new EmailEntry { Id = 3, Email = "a@b.com" }
+            };
 
-            // LINQ query to find duplicates
             var duplicateEmails = people
                 .GroupBy(p => p.Email)
                 .Where(g => g.Count() > 1)
                 .Select(g => g.Key);
 
-            // Output results
             Console.WriteLine("Duplicate Emails:");
             foreach (var email in duplicateEmails)
-            {
                 Console.WriteLine(email);
-            }
         }
     } 
 }
